bef4dea24e399713bc15413c3fa43fec4873908a
delimited0 <patrickding00@gmail.com>
2022-04-26 10:26:11 -0500

    install gcKrig thru devtools

--- install_github_packages.R
+++ install_github_packages.R
@@ -2,10 +2,12 @@ library(devtools)
 
 # tmg no longer updated
 devtools::install_github("cran/tmg")
+devtools::install_github("cran/gcKrig")
 
 # custom packages
 install_github("delimited0/epmgpr")
 install_github("delimited0/sampletmvn")
 install_github("delimited0/met")
 install_github("delimited0/rcpp-lin-con-gauss")
-install_github("JCatwood/HCCMVN")
+
+# install_github("JCatwood/HCCMVN")
