f603683ea96fbb017329b29976a56afc84af4d0a
delimited0 <patrickding00@gmail.com>
2022-05-05 22:23:18 -0500

    typo in conf

--- experiments/prob_cmpdsymm_orthant/hierarchical_cmpdsymm_orthant.sh
+++ experiments/prob_cmpdsymm_orthant/hierarchical_cmpdsymm_orthant.sh
@@ -2,7 +2,7 @@
 
 Rscript experiments/prob_cmpdsymm_orthant/prob_cmpdsymm_orthant.R \
 --method_conf=experiments/prob_cmpdsymm_orthant/hierarchical_conf.json \
---dim_conf=experiments/prob_cmpdsymm_orthant/hierachical_dim.json \
+--dim_conf=experiments/prob_cmpdsymm_orthant/hierarchical_dim.json \
 --corr=.5 \
 --result_path=experiments/prob_cmpdsymm_orthant/results \
 --seed=2022 \
