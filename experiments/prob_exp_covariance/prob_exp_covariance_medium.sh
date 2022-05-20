8ba6d7ab1f433768e98f8b417b60001a651de1f3
delimited0 <patrickding00@gmail.com>
2022-05-20 01:40:30 -0500

    divide by problem size

--- experiments/prob_exp_covariance/prob_exp_covariance_medium.sh
+++ experiments/prob_exp_covariance/prob_exp_covariance_medium.sh
@@ -2,7 +2,7 @@
 
 Rscript experiments/prob_exp_covariance/prob_exp_covariance.R \
 --method_conf=experiments/prob_exp_covariance/method_conf.json \
---dim_conf=experiments/prob_exp_covariance/dim_conf.json \
+--dim_conf=experiments/prob_exp_covariance/dim_medium.json \
 --dep=.1 \
 --result_path=experiments/prob_exp_covariance/results \
 --n_reps=18 \
